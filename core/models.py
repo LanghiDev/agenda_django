1b4c018c7e5c6f277067596e00f1b6fcdefb9d20
Nicolas Ceschin Langhi <nicolasclanghi@gmail.com>
2021-04-16 14:28:04 -0300

    Criado tela de listagem de eventos

--- core/models.py
+++ core/models.py
@@ -14,3 +14,9 @@ class Evento(models.Model):
 
     def __str__(self):
         return self.titulo
+
+    def get_data_evento(self):
+        return self.data_evento.strftime('%d/%m/%Y %H:%M')
+
+    def get_data_criacao(self):
+        return self.data_criacao.strftime('%d/%m/%Y')
